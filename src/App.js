768274f9c72f52ba5f4cee5384029f2bc20a5be6
sebastienjj <josephsebastien.sj@gmail.com>
2022-03-14 15:36:11 -0400

    added my component, src etc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import { Article, Brand, Cta} from 
+import { Footer, Blog, Features, WhatApp, Header } from './containers'
+import { Cta, Brand, Navbar } from './components'
+import './App.css'
 
-function App() {
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className = "App">
+    <div className = "gradient__bg">
+      <Navbar />
+      <Header />
+      </div>
+      <Brand />
+      <WhatApp />
+      <Features />
+      {/* <Possibility /> */}
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/whatApp/WhatApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './whatApp.css'
+
+const WhatApp = () => {
+  return (
+    <div>WhatApp</div>
+  )
+}
+
+export default WhatApp
