1f1b90f5b7f0c2c1b6e7b5e52eb8bd119590c302
sebastienjj <josephsebastien.sj@gmail.com>
2022-03-14 16:12:04 -0400

    added assest with some gradients using add ons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { RiMenu3line, Ricloseline } from ' react-icons/ri'
 import './navbar.css'
+
+//BEM block element modifier
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="whatApp__navbar-">
+    <div className="whatApp__navbar-links">
+      <div className="whatApp__navbar-links_logo">
+        <img src={logo} alt="logo" />
+      </div>
+      </div>
+      </div>
   )
 }
 
