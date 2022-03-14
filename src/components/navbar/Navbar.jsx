9a9f5b7c15643f9436cc5d9229501fc391fda807
sebastienjj <josephsebastien.sj@gmail.com>
2022-03-14 17:08:07 -0400

    added some new features

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { RiMenu3line, Ricloseline } from ' react-icons/ri'
+// import { RiMenu3line, Ricloseline } from ' react-icons/ri'
+import logo from '../../assets/logo.svg'
 import './navbar.css'
 
 //BEM block element modifier
@@ -10,6 +11,13 @@ const Navbar = () => {
     <div className="whatApp__navbar-links">
       <div className="whatApp__navbar-links_logo">
         <img src={logo} alt="logo" />
+      </div>
+      <div className="whatApp__navbar-links_contanier">
+      <p><a href="#home"> Home </a></p>
+      <p> <a href="#whatApp"> what is Sol? </a></p>
+      <p> <a href="#home"> Home </a></p>
+      <p> <a href="#features"> Case Studies </a></p>
+      <p> <a href="#blog"> library </a></p>
       </div>
       </div>
       </div>
