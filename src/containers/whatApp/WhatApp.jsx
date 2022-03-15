dce5ae39db8731c96864921a6b6a23a6bc4cf95a
sebastienjj <josephsebastien.sj@gmail.com>
2022-03-14 22:25:12 -0400

    clean up

--- src/containers/whatApp/WhatApp.jsx
+++ src/containers/whatApp/WhatApp.jsx
@@ -5,7 +5,7 @@ import './whatApp.css';
 const WhatApp = () => (
   <div className="App__whatApp section__margin" id="App">
     <div className="App__whatApp-feature">
-      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      {/* <Feature title="What is SOL" text="" /> */}
     </div>
     <div className="App__whatApp-heading">
       <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
